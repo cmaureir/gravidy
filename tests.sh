c32ff7cb5283398a62bd421178c9012f801cff4a
Cristian Maureira Fredes <cmaureir@csrg.inf.utfsm.cl>
2012-09-14 16:06:04 +0200

    Adding gitignore

--- tests.sh
+++ tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for i in $(/bin/ls -1 input/ | grep -v py);
 do
-    echo $i >> CPU_E
-    echo $i >> GPU_E
-     ./gravidy -i input/$i -t 1 -r cpu >> CPU_E
-     ./gravidy -i input/$i -t 1 -r gpu >> GPU_E
+    echo $i >> $i.cpu.out
+     ./gravidy -i input/$i -t 1 -r cpu &>> $i.cpu.out
+    echo $i >> $i.gpu.out
+     ./gravidy -i input/$i -t 1 -r gpu &>> $i.gpu.out
 done
