59704ce21b20a7a796bf39c55131630a08325c95
Cristian Maureira Fredes <cmaureir@csrg.inf.utfsm.cl>
2012-10-24 16:15:19 +0200

    Fixing little error on the execution time script

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 from subprocess import call
 
-files = ['04-nbody-p1024_m1.in', \
-        'nbody-p256.in',  \
-        'nbody-p128.in',  \
-        '05-nbody-p2048_m1.in']
+files = []
+
+files.append('input/01-nbody-p128_m1.in')
+files.append('input/02-nbody-p256_m1.in')
+files.append('input/03-nbody-p512_m1.in')
+files.append('input/04-nbody-p1024_m1.in')
+files.append('input/05-nbody-p2048_m1.in')
+files.append('input/06-nbody-p4096_m1.in')
+files.append('input/07-nbody-p8192_m1.in')
+files.append('input/08-nbody-p16384_m1.in')
 
 for f in files:
     print f
-    st_cpu = call("./gravidy -i input/"+f+" -t 1 -r cpu &> output_"+f, shell=True)
+    st_cpu = call("time ./gravidy -i "+f+" -t 1", shell=True)
     if st_cpu != 0:
         print "Error with CPU version"
-#    st_gpu = call("./gravidy -i input/"+f+" -t 1 -r gpu", shell=True)
-#    if st_gpu != 0:
-#        print "Error with CPU version"
