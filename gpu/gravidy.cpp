17599652dc5b122b4776bf383e660843d5f38a44
Cristian Maureira Fredes <cmaureir@csrg.inf.utfsm.cl>
2013-03-04 17:24:35 -0300

    Fixing timestamp

--- gpu/gravidy.cpp
+++ gpu/gravidy.cpp
@@ -73,7 +73,7 @@ string getTime ()
     time(&timeObj);
     tm *pTime = gmtime(&timeObj);
     char buffer[100];
-    sprintf(buffer, "%d-%d-%d_%d:%d:%d", pTime->tm_mday, pTime->tm_mon, 1900+pTime->tm_year, pTime->tm_hour, pTime->tm_min, pTime->tm_sec);
+    sprintf(buffer, "%02d-%02d-%04d_%02d:%02d:%02d", pTime->tm_mday, pTime->tm_mon, 1900+pTime->tm_year, pTime->tm_hour, pTime->tm_min, pTime->tm_sec);
     return buffer;
 }
 
