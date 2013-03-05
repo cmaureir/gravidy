e982d9eb9d0a5eb7cce45eca9e8da92a5993e80e
Cristian Maureira Fredes <cmaureir@csrg.inf.utfsm.cl>
2013-03-05 11:41:21 -0300

    Fixing typo in the cluster scripts

--- tests/cluster/eta/eta_01024.sh
+++ tests/cluster/eta/eta_01024.sh
@@ -9,7 +9,7 @@ etas=(0.001 0.003 0.005 0.007 0.01 0.03 0.05 0.07 0.1)
 
 cd /user/c/cmaureir/GraviDy/
 
-for e in "${etas[@]}
+for e in "${etas[@]}"
 do
     (time gpu/./gravidy -i input/$file -t 1 -e $e)
 done
