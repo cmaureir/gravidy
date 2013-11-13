fdf7b49f5ea4ceb43e95ad6405d0928cdb905ee7
Cristian Maureira-Fredes <cmaurei@aei.mpg.de>
2013-11-13 14:15:02 +0100

    Adding comments to main
    Signed-off-by: Cristian Maureira-Fredes <cmaurei@aei.mpg.de>

--- src/new-oo/gravidy.cpp
+++ src/new-oo/gravidy.cpp
@@ -1,5 +1,12 @@
 #include "include/NbodySystem.hpp"
 
+/*
+ * Main routing of the initialization and exeucutio of GraviDy
+ *
+ * The main objects based on the existing classes are created here
+ * and passing to some methods to perform the interaction of them
+ * to achieve an N-body system integration.
+ */
 int main(int argc, char *argv[]) {
 
     // Parsing the command-line parameters
@@ -9,26 +16,36 @@ int main(int argc, char *argv[]) {
     // Initialization of the system
     NbodySystem ns(op);
 
+    // Reading the input file, storing the information in a temporal structure
     ns.read_input_file();
+
+    // Memory allocation on the Host and Device
     ns.alloc_arrays_host();
     #ifdef GPU
     ns.alloc_arrays_device();
     #endif
+
+    // Copy the data from the temporal structure to the Host variables
     ns.copy_initial_data();
 
+    // Creating an Hermite object depending on the compiling rules
     #ifdef GPU
     Hermite4GPU h4(ns.n, ns.e2, ns.eta);
     #else
     Hermite4CPU h4(ns.n, ns.e2, ns.eta);
     #endif
 
+    // Creating the object in charge of printing the program logs
     Logger log(ns.print_log, ns.out_file);
+    // Creating the object with some extra functionallity for N-body codes
     NbodyUtils nu(ns.n, ns.h_r, ns.total_mass);
 
+    // Calling the integration process using the corresponding Hermite object
     ns.gtime.integration_ini = omp_get_wtime();
     ns.integration(h4, log, nu);
     ns.gtime.integration_end = omp_get_wtime();
 
+    // Freeing memory on the Device and Host
     #ifdef GPU
     ns.free_arrays_device();
     #endif
