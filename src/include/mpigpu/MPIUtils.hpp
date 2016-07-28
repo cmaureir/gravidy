9ce5470d84b4258a5e7c7047a70b7b335212e295
Cristian Maureira-Fredes <cmaurei@aei.mpg.de>
2016-07-28 20:55:01 +0200

    Removing devs directories
    Signed-off-by: Cristian Maureira-Fredes <cmaurei@aei.mpg.de>

--- src/include/mpigpu/MPIUtils.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef MPI_UTILS
-#define MPI_UTILS
-#include "Hermite4MPIGPU.cuh"
-
-void define_forces_struct(MPI_Datatype *ts);
-void forces_operation(void *in, void *inout, int *len, MPI_Datatype *type);
-
-#endif // MPI_UTILS
